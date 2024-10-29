5bae69ac553f8737013ac82f8a37abf5a22369dd
LautaroSettembrini <lautaronehuensettembrini@gmail.com>
2024-10-29 16:15:10 -0300

    1.08

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const xmlbuilder = require('xmlbuilder');
+const jwtDecode = require('jwt-decode');
 require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
 
 const app = express();
